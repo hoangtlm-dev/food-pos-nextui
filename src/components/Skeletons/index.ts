94e9408d2fcd3d4904cf0998f523ab7c6d1d4400
hoangtlm-dev <hoangtlm2410@gmail.com>
2024-10-18 23:35:21 +0700

    feat: implement ui & storybook for skeleton inventory food card

--- src/components/Skeletons/index.ts
+++ src/components/Skeletons/index.ts
@@ -1,2 +1,3 @@
 export { default as SkeletonCustomerFoodCard } from './SkeletonCustomerFoodCard'
 export { default as SkeletonCustomerFoodList } from './SkeletonCustomerFoodList'
+export { default as SkeletonInventoryFoodCard } from './SkeletonInventoryFoodCard'
